c8206a039f5a9e3d15ddf8d82d1bb7ef144ccb8a
Dmitry Zakharov <dmitry.zakharov.v@outlook.com>
2022-06-18 11:37:24 +1100

    HW terraform-04-teamwork (terraform_cloud, atlantis)

---
+++ 2.Virt-homeworks/07-terraform-04-teamwork/src/main.tf
@@ -0,0 +1,49 @@
+provider yandex {
+  token  = var.yc_token
+  cloud_id  = var.yandex_cloud_id
+  folder_id = var.yandex_folder_id
+  
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd87tirk5i8vitv9uuo1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+    
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
